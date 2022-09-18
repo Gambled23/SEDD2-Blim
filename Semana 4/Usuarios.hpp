07498a0c7e59bea697214790e1c8825d46ae97d6
Cesar <ipog71@gmail.com>
2022-09-18 12:10:15 -0500

    prototipo usuarios

---
+++ Semana 4/Usuarios.hpp
@@ -0,0 +1,53 @@
+#ifndef USUARIOS_H
+#define USUARIOS_H
+#include <iostream>
+#include <cstdlib>
+#pragma once
+
+using namespace std;
+
+class Usuarios
+{
+public:
+    Usuarios();
+    string nombre;
+    string estadoServicio;
+    string tarjeta;
+    string domicilio;
+    string inicioServicio;
+
+    void agregar();
+    void imprimir();
+    void buscar();
+    void modificar();
+    void eliminar();
+private:
+
+};
+
+Usuarios::Usuarios()
+{
+
+}
+
+void Usuarios::agregar()
+{
+
+}
+void Usuarios::imprimir()
+{
+
+}
+void Usuarios::buscar()
+{
+
+}
+void Usuarios::modificar()
+{
+
+}
+void Usuarios::eliminar()
+{
+
+}
+#endif
