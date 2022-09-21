07a6d79985765db1929cd16981ed4cb42942e4b6
Cesar <ipog71@gmail.com>
2022-09-21 09:47:59 -0500

    puto programa de mierda

---
+++ Semana 4/usuarios/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+#include "users.hpp"
+
+using namespace std;
+int main()
+{
+    users usuario1;
+    usuario1.Capturar();
+    //usuario1.Mostrar();
+    system("PAUSE");
+    return 0;
+}
